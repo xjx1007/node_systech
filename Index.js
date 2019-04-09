122fe464c39c151e5a6a5d0b348e5b2e21387595
sg <sg sg@DESKTOP-2N71COR>
2019-04-09 11:26:46 +0800

    no message

--- Index.js
+++ Index.js
@@ -74,9 +74,9 @@ app.get('/OperationDevice',function(request,response){
     response.end();
 })
 /* 监听请求supervisor */
-/* var server = app.listen(8082, function () {
+var server = app.listen(8082, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("已启动监听，访问地址为 http://%s:%s", host, port)
 
-}) */
+})
